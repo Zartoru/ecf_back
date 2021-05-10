9b145a33288f7aaca0b28673c7e2b00544b52ebe
zartoru <bassez.claude97@gmail.com>
2021-05-10 16:22:45 +0200

    ajout de schoolyears.html ajout d'une connection a la bdd pour afficher les données des school years

--- data_access/views.py
+++ data_access/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from ecf_bdd.models import SchoolYear
 
 # Create your views here.
+def index(request):
+    return render(request, 'data_access/index.html')
+
+def schoolyears(request):
+    school_years = SchoolYear.objects.all()
+    data = {'school_years': school_years}
+
+    return render(request, 'data_access/schoolyears.html', data)
--- ecf_acces_donnees/settings.py
+++ ecf_acces_donnees/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ecf_bdd.apps.EcfBddConfig'
+    'ecf_bdd.apps.EcfBddConfig',
+    'data_access.apps.DataAccessConfig'
 ]
 
 MIDDLEWARE = [
